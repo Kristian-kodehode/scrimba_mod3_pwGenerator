985bc99f10bb307b491554ceebbad0beb0d58166
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-04-05 12:15:24 +0200

    doing more tasks from ai

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-for (let i = 0; i <= 20; i++) {
-  console.log(i);
+let question = prompt("Give me a number please: ");
+if (question > 0) {
+  console.log("Positive");
+} else if (question < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
 }
