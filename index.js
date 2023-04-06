421a8bd45d284739e76c4e61b282685a6ef19f13
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-04-06 14:31:38 +0200

    Practice objects with if statements thar are used in a function

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const car = {
-  make: "Ford",
-  model: "GT40",
-  year: 1972,
-  color: "Sky-blue",
-  mileage: 1200,
+const book = {
+  title: "Striker",
+  author: "Joan Book",
+  pageCount: 240,
+  isRead: true,
 };
 
-function drive(car) {
+function readBook(book) {
+  let readStatus;
+  if (book.isRead) {
+    readStatus = "read";
+  } else {
+    readStatus = "not read";
+  }
   console.log(
-    "The " +
-      car.make +
-      car.model +
-      " is driving. The mileage is now " +
-      car.mileage
+    "I have " +
+      readStatus +
+      " the book " +
+      book.title +
+      " by " +
+      book.author +
+      ". It has " +
+      book.pageCount +
+      " pages."
   );
 }
 
-drive(car);
+readBook(book);
