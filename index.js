d4c89eaaaaef7e600efa2caad95f5bd5e3752a3c
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-04-06 19:59:13 +0200

    Trying to seperate numbers, letters and characters

--- index.js
+++ index.js
@@ -1,66 +1,4 @@
-const characters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
+const listCharacters = [
   "~",
   "`",
   "!",
@@ -92,17 +30,77 @@ const characters = [
   "/",
 ];
 
+const listLettersUpper = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const listLettersLower = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const listNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 function randomGenerator() {
-  const randomIndex = Math.floor(Math.random() * characters.length);
-  return characters[randomIndex];
+  const randomIndex = Math.floor(Math.random() * listNumbers.length);
+  return listNumbers[randomIndex];
 }
 
 function generatePassword(length) {
-  let password = "";
+  let listNumbers = "";
   for (let i = 0; i < length; i++) {
-    password += randomGenerator();
+    listNumbers += randomGenerator();
   }
-  return password;
+  return listNumbers;
 }
 
 console.log(generatePassword(10));
