2470bc84f8e420c46a9442f142d725d34574ddd4
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-04-06 20:59:28 +0200

    Fixed the js solution from getting tips from AI

--- index.js
+++ index.js
@@ -1,104 +1,115 @@
-function randomGenerator(listCombined) {
-  const randomIndex = Math.floor(Math.random() * listCombined.length);
-  return listCombined[randomIndex];
+const listLettersLower = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const listLettersUpper = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+const listNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const listCharacters = [
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let randomPasswordGenerator = "";
+
+for (let i = 0; i < 1; i++) {
+  const randomLetterU = Math.floor(Math.random() * listLettersUpper.length);
+  randomPasswordGenerator += listLettersUpper[randomLetterU];
 }
 
-function generatePassword(length) {
-  let listCombined = "";
-  const listLettersLower = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  const listLettersUpper = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  const listNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  const listCharacters = [
-    "~",
-    "`",
-    "!",
-    "@",
-    "#",
-    "$",
-    "%",
-    "^",
-    "&",
-    "*",
-    "(",
-    ")",
-    "_",
-    "-",
-    "+",
-    "=",
-    "{",
-    "[",
-    "}",
-    "]",
-    ",",
-    "|",
-    ":",
-    ";",
-    "<",
-    ">",
-    ".",
-    "?",
-    "/",
-  ];
-  const allCharacters =
-    listLettersLower + listLettersUpper + listNumbers + listCharacters;
-  for (let i = 0; i < length; i++) {
-    listCombined += randomGenerator(allCharacters);
-  }
-  return listCombined;
+for (let i = 0; i < 12; i++) {
+  const randomLetterL = Math.floor(Math.random() * listLettersLower.length);
+  randomPasswordGenerator += listLettersLower[randomLetterL];
 }
 
-console.log(generatePassword(15));
+for (let i = 0; i < 1; i++) {
+  const randomNumber = Math.floor(Math.random() * listNumbers.length);
+  randomPasswordGenerator += listNumbers[randomNumber];
+}
+
+for (let i = 0; i < 1; i++) {
+  const randomCharacter = Math.floor(Math.random() * listCharacters.length);
+  randomPasswordGenerator += listCharacters[randomCharacter];
+}
+
+console.log(randomPasswordGenerator);
