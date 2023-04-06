c82593b44ee798d72e391b75359fba6310c8e50f
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-04-06 20:30:15 +0200

    Getting help from chat gpt

--- index.js
+++ index.js
@@ -1,106 +1,104 @@
-const listCharacters = [
-  "~",
-  "`",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "{",
-  "[",
-  "}",
-  "]",
-  ",",
-  "|",
-  ":",
-  ";",
-  "<",
-  ">",
-  ".",
-  "?",
-  "/",
-];
-
-const listLettersUpper = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-
-const listLettersLower = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-
-const listNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-function randomGenerator() {
-  const randomIndex = Math.floor(Math.random() * listNumbers.length);
-  return listNumbers[randomIndex];
+function randomGenerator(listCombined) {
+  const randomIndex = Math.floor(Math.random() * listCombined.length);
+  return listCombined[randomIndex];
 }
 
 function generatePassword(length) {
-  let listNumbers = "";
+  let listCombined = "";
+  const listLettersLower = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const listLettersUpper = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const listNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const listCharacters = [
+    "~",
+    "`",
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "-",
+    "+",
+    "=",
+    "{",
+    "[",
+    "}",
+    "]",
+    ",",
+    "|",
+    ":",
+    ";",
+    "<",
+    ">",
+    ".",
+    "?",
+    "/",
+  ];
+  const allCharacters =
+    listLettersLower + listLettersUpper + listNumbers + listCharacters;
   for (let i = 0; i < length; i++) {
-    listNumbers += randomGenerator();
+    listCombined += randomGenerator(allCharacters);
   }
-  return listNumbers;
+  return listCombined;
 }
 
-console.log(generatePassword(10));
+console.log(generatePassword(15));
