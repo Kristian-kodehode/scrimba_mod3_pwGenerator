1064dc7f4ea63b323d2fbff224bfeb2f9069b31e
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-04-06 18:00:38 +0200

    trying out different things with objects and arrays

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const cars = [0, 1, 2, 3];
+cars[0] = "volvo";
+cars[1] = "Ford";
+cars[2] = "Mercedes";
+cars[3] = "Opel";
+
 const car = {
-  make: "Ferrari ",
+  make: cars[0],
   model: "F40",
   year: 1994,
 };
@@ -16,11 +22,3 @@ function presentingCard(car) {
 }
 
 presentingCard(car);
-
-const cars = [0, 1, 2, 3];
-cars[0] = "volvo";
-cars[1] = "Ford";
-cars[2] = "Mercedes";
-cars[3] = "Opel";
-
-console.log(cars[2]);
